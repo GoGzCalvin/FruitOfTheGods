8d4438200aec35643a84859e27c45fee95331609
s179068 <s179068@SEA-8E412E>
2017-09-05 17:35:42 -0700

    Operation Health

--- FruitOfTheGods/Main.cpp
+++ FruitOfTheGods/Main.cpp
@@ -9,22 +9,21 @@ int main()
 	characters player;
 
 	player.health = 100;
-	player.damage = 15;
-	player.defense = 10;
+	player.damage = 20;
+	player.defense = 15;
 	player.heal = 30;
 
 
 	characters enemy;
 
 	enemy.health = 150;
-	enemy.damage = 20;
+	enemy.damage = 25;
 	enemy.defense = 5;
 	enemy.heal = 15;
 
-	introduction();
+	introduction(player);
 	battle(player, enemy);
-
-
+	afterBattle(player);
 
 
 	//battle(player, ogre);
--- FruitOfTheGods/characters.h
+++ FruitOfTheGods/characters.h
@@ -5,12 +5,12 @@ using namespace std;
 
 struct characters
 {
+	char name[50];
 	int health;
 	int damage;
 	int heal;
 	int healthRemaining;
 	double defense;
-
 };
 
 void DealDamage(characters &player, characters &enemy)
@@ -18,16 +18,46 @@ void DealDamage(characters &player, characters &enemy)
 	player.health -= enemy.damage;
 	enemy.health -= player.damage;
 
+	// Player Attack
+	DelayText(25, "You attacked and dealt ");
+	ColorPicker(11);
+	cout << player.damage;
+	ColorPicker(15);
+	DelayText(25, " damage. The enemy has ");
+	ColorPicker(12);
+	cout << enemy.health;
+	ColorPicker(15);
+	DelayText(25, " health remaining.");
+	cout << "\n";
 
-	cout << "You attacked and dealt  " << player.damage << " damage. The enemy has " << enemy.health << " health remaining." << endl;
-	cout << "The enemy attacked and dealt " << enemy.damage << " damage. The player has " << player.health << " health remaining." << endl;
+
+
+	// Enemy Attack
+	DelayText(25, "The enemy attacked and dealt ");
+	ColorPicker(12);
+	cout << enemy.damage;
+	ColorPicker(15);
+	DelayText(25, " damage. The player has ");
+	ColorPicker(10);
+	cout << player.health;
+	ColorPicker(15);
+	DelayText(25, " health remaining.");
+	cout << "\n";
 }
 
 void Heal(characters &player)
 {
 	player.healthRemaining = (player.health + player.heal);
 	
-	cout << "You have healed " << player.heal << " damage." << "You have " << player.healthRemaining << " health remaining." << endl;
+	DelayText(25, "You healed ");
+	ColorPicker(10);
+	cout << player.heal;
+	ColorPicker(15);
+	DelayText(25, " damage. You have ");
+	ColorPicker(10);
+	cout << player.healthRemaining;
+	ColorPicker(15);
+	DelayTextWithSkip(25, " health remaining. \n");
 }
 
 void Defend(characters &player, characters &enemy)
@@ -36,46 +66,156 @@ void Defend(characters &player, characters &enemy)
 
 	player.health -= player.defense;
 
-	cout << "The enemy attacked and dealt " << player.defense << " damage. The player has " << player.health << " health remaining." << endl;
+	DelayText(25, "The enemy attacked and dealt ");
+	ColorPicker(12);
+	cout << player.defense;
+	ColorPicker(15);
+	DelayText(25, " damage. ");
+	DelayText(25, "The player has ");
+	ColorPicker(10);
+	cout << player.health;
+	ColorPicker(15);
+	DelayTextWithSkip(25, " health remaining.");
+}
+
+void playerStats(characters &player)
+{
+	
+	cout << "Health. ";
+	ColorPicker(10);
+	cout << player.health << endl;
+	ColorPicker(15);
+
+	cout << "Attack. ";
+	ColorPicker(12);
+	cout << player.damage << endl;
+	ColorPicker(15);
+
+	cout << "Defense. ";
+	ColorPicker(11);
+	cout << player.defense << endl;
+	ColorPicker(15);
+
+
+}
+
+void enemyStats(characters &enemy)
+{
+
+	cout << "Health. ";
+	ColorPicker(10);
+	cout << enemy.health << endl;
+	ColorPicker(15);
+
+	cout << "Attack. ";
+	ColorPicker(12);
+	cout << enemy.damage << endl;
+	ColorPicker(15);
+
+	cout << "Defense. ";
+	ColorPicker(11);
+	cout << enemy.defense << endl;
+	ColorPicker(15);
+
+
 }
 
 
-void introduction()
+void introduction(characters &player)
 {
 	ColorPicker(14);
-	char playerName[20];
-
-	/*cout << "Fruit of the Gods!" << endl;*/
+	char playerName[25];
 	
-	DelayTextWithSkip(20, "Welcome to Fruit of the Gods!");
-	//ColorPicker(10);
-	DelayTextWithSkip(20, "You have awoken...");
-	DelayTextWithSkip(20, "Your celestial powers are intact, however your angel wings will not appear...");
-	DelayTextWithSkip(20, "You recall your betrayal to your angelic kin to set out on your quest to become the strongest being in existence a God with angelic power.");
-	DelayTextWithSkip(20, "Although you can't seem to remember your name, what was it again?");
+	DelayTextWithSkip(25, "Welcome to Fruit of the Gods! \n");
+	DelayTextWithSkip(25, "You have awoken... \n");
+	DelayTextWithSkip(25, "Your celestial powers are intact, however your angel wings will not appear... \n");
+	DelayTextWithSkip(25, "You recall your betrayal to your angelic kin to set out on your quest to become the strongest being in existence a God with angelic power. \n");
+	DelayTextWithSkip(25, "Although you can't seem to remember your name, what was it again? \n");
 
-	cin >> playerName;
 
-	char buffer[80] = "Hello ";
-	strcat_s(buffer, playerName);
-	DelayTextWithSkip(20, buffer);
-
+	ColorPicker(11);
+	cin >> player.name;
+	cout << "\n";
+	char buffer[80] = "Hello, ";
+	strcat_s(buffer, player.name);
+	cout << "\n";
+	DelayTextWithSkip(15, buffer);
+	cout << "\n";
 
 	
+}
+
+void afterBattle(characters player)
+{
+	ColorPicker(12);
+	char playerName[25];
+	
+
+	DelayText(25, "Well done ");
+	ColorPicker(11);
+	DelayText(25,player.name);
+	cout << "\n";
+	ColorPicker(12);
+	DelayTextWithSkip(25, "You have grown since your betrayal to the angels. \n");
+	DelayTextWithSkip(25, "Our brothers and sisters assumed you would have lost your grace after betraying us. \n");
+	DelayTextWithSkip(25, "Although it appears your wings have been revoked to prevent you from returning to the celestial plain. \n");
+	DelayText(25, "We shall meet again \n");
+	ColorPicker(11);
+	DelayText(25, player.name);
+	ColorPicker(15);
+
+
+
+
+
 }
 
 void battle(characters &player, characters & enemy)
 {
-	ColorPicker(12);
+	ColorPicker(15);
 	int input;
 
-	DelayTextWithSkip(20, "Your superior Diana has confronted you. ");
-
+	DelayText(25, "You have been challenged by your superior ");
+	ColorPicker(12);
+	DelayTextWithSkip(25, "Altair.");
+	
 	do
 	{
-		DelayTextWithSkip(20, "Input '1' to attack.");
-		DelayTextWithSkip(20, "Input '2' to heal");
-		DelayTextWithSkip(20, "Input '3' to defend.");;
+		// Attack
+		ColorPicker(15);
+		cout << "Input '1' to ";
+		ColorPicker(12);
+		cout << " attack \n";
+		ColorPicker(15);
+
+
+		// Heal
+		cout << "Input '2' to ";
+		ColorPicker(10);
+		cout <<	" heal \n";
+
+
+		// Defend
+		ColorPicker(15);
+		cout << "Input '3' to ";
+		ColorPicker(11);
+		cout << " defend \n";
+		ColorPicker(15);
+
+
+		// Player Stats
+		ColorPicker(15);
+		cout << "Input '4' to ";
+		ColorPicker(14);
+		cout << " view player stats \n";
+		ColorPicker(15);
+
+
+		//EnemyStats
+		cout << "Input '5' to ";
+		ColorPicker(12);
+		cout << " view enemy stats \n";
+		ColorPicker(15);
 
 
 		cin >> input;
@@ -95,15 +235,25 @@ void battle(characters &player, characters & enemy)
 			Defend(player, enemy);
 		}
 
+		else if (input == 4)
+		{
+			playerStats(player);
+		}
+
+		else if (input == 5)
+		{
+			enemyStats(enemy);
+		}
+
 
 		else if (player.health < 0)
 		{
-			cout << "Player has died." << endl;
+			DelayTextWithSkip(25, "You have been defeated.");
 		}
 
 		else if (enemy.health < 0)
 		{
-			cout << "The enemy has died." << endl;
+			DelayTextWithSkip(25, "The enemy has been defeated.");
 		}
 
 		else
